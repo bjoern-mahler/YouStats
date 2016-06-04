d564a0bcf165a923b7612d828ecc474d1bf5eb71
Björn Mahler <bjomahler@gmail.com>
2016-06-04 14:36:20 +0200

    setup project

--- src/main/java/de/mahler/youstats/WebConfiguration.java
+++ src/main/java/de/mahler/youstats/WebConfiguration.java
@@ -3,7 +3,6 @@ package de.mahler.youstats;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 public class WebConfiguration {
